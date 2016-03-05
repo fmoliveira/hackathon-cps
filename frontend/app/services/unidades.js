4b635c05fe8812300ef00a9420bdb9033def2c97
Roberto Júnior <robertoco.jr@gmail.com>
2016-03-05 13:22:44 -0300

    PAdronização Unidades

--- frontend/app/services/unidades.js
+++ frontend/app/services/unidades.js
@@ -9,14 +9,18 @@ require('angular')
 angular.module('HackathonApp')
 
 /* Define o serviço para retornar a lista de unidades */
-.factory('ListaUnidades', function(){
-  var unidades = []
+.factory('Unidades', function(){
+  var service = {}
 
-  /* Dados mock */
-  unidades.push({id: 1, nome: 'CENTRO DE REABILITACAO FISICA', endereco: 'Endereço 1', melhorHorario: '12h'})
-  unidades.push({id: 2, nome: 'CENTRO DE SAUDE BOA VISTA', endereco: 'Endereço 2', melhorHorario: '13h'})
-  unidades.push({id: 3, nome: 'CENTRO DE SAUDE BARAO GERALDO', endereco: 'Endereço 3', melhorHorario: '14h'})
-  unidades.push({id: 4, nome: 'CENTRO DE SAUDE BOA ESPERANCA', endereco: 'Endereço 4', melhorHorario: '15h'})
+  service.listarUnidades = function(){
+    var model = [
+    {id: 1, nome: 'CENTRO DE REABILITACAO FISICA', endereco: 'Endereço 1', melhorHorario: '12h'},
+    {id: 2, nome: 'CENTRO DE SAUDE BOA VISTA', endereco: 'Endereço 2', melhorHorario: '13h'},
+    {id: 3, nome: 'CENTRO DE SAUDE BARAO GERALDO', endereco: 'Endereço 3', melhorHorario: '14h'},
+    {id: 4, nome: 'CENTRO DE SAUDE BOA ESPERANCA', endereco: 'Endereço 4', melhorHorario: '15h'}
+    ]
+    return model
+  }
 
-  return unidades
+  return service
 })
