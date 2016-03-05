749e09ea54de4841a7e9cd26c25d360661cbf3f9
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-05 13:17:39 -0300

    Refactor do mock de unidades de atendimento.

--- frontend/app/services/unidades.js
+++ frontend/app/services/unidades.js
@@ -12,15 +12,16 @@ angular.module('HackathonApp')
 .factory('Unidades', function(){
   var service = {}
 
+  var mock = [
+    {id: 1, nome: 'CENTRO DE REABILITACAO FISICA', endereco: 'Endereço 1', melhorHorario: '12h'},
+    {id: 2, nome: 'CENTRO DE SAUDE BOA VISTA', endereco: 'Endereço 2', melhorHorario: '13h'},
+    {id: 3, nome: 'CENTRO DE SAUDE BARAO GERALDO', endereco: 'Endereço 3', melhorHorario: '14h'},
+    {id: 4, nome: 'CENTRO DE SAUDE BOA ESPERANCA', endereco: 'Endereço 4', melhorHorario: '15h'}
+  ]
+
   /* Dados mock */
   service.listarUnidades = function () {
-    var model = [
-      {id: 1, nome: 'CENTRO DE REABILITACAO FISICA', endereco: 'Endereço 1', melhorHorario: '12h'},
-      {id: 2, nome: 'CENTRO DE SAUDE BOA VISTA', endereco: 'Endereço 2', melhorHorario: '13h'},
-      {id: 3, nome: 'CENTRO DE SAUDE BARAO GERALDO', endereco: 'Endereço 3', melhorHorario: '14h'},
-      {id: 4, nome: 'CENTRO DE SAUDE BOA ESPERANCA', endereco: 'Endereço 4', melhorHorario: '15h'}
-    ]
-    return model
+    return mock
   }
 
   return service
