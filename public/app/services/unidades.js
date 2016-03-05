515f6f0b508ef6a61a2ea4f198cebb5747fdc845
Roberto Júnior <robertoco.jr@gmail.com>
2016-03-05 18:00:33 -0300

    chart unidade

--- public/app/services/unidades.js
+++ public/app/services/unidades.js
@@ -22,12 +22,41 @@ angular.module('HackathonApp')
     { id: 3, regiao: 'Leste', nome: 'CENTRO DE SAUDE BARAO GERALDO', endereco: 'Endereço 3', melhorHorario: '14h' },
     { id: 4, regiao: 'Sul', nome: 'CENTRO DE SAUDE BOA ESPERANCA', endereco: 'Endereço 4', melhorHorario: '15h' }
   ]
+  var mockChart = {
+    labels: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [6, 10, 15, 20, 35, 40, 36, 35, 34, 22, 23, 14, 17, 12]
+      },
+      {
+        label: 'My Second dataset',
+        fillColor: 'rgba(151,187,205,0.2)',
+        strokeColor: 'rgba(151,187,205,1)',
+        pointColor: 'rgba(151,187,205,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(151,187,205,1)',
+        data: [9, 15, 14, 5, 23, 34, 41, 29, 23, 33, 34, 21, 25, 18]
+      }
+    ]
+  }
 
   /* Consulta a lista de unidades */
   service.listarUnidades = function (regiao) {
     return _.filter(mock, function (i) {
       return (i.regiao === regiao)
     })
+  }
+
+  service.chartUnidade = function (unidades) {
+    return mockChart
   }
 
   return service
