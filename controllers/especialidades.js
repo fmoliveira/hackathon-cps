41aa15d96a66a7932119c217e818d0b35421c850
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-06 03:24:26 -0300

    Comentando o código.

--- controllers/especialidades.js
+++ controllers/especialidades.js
@@ -3,19 +3,24 @@
 const express = require('express')
 const _ = require('lodash')
 
+/* Dados de saúde do banco de dados do Saúde Campinas */
 const Saude = require('../models/saude')
 
+/* Inicializa o roteamento */
 exports = express.Router()
 
+/* Consulta a listagem de especialidades médicas */
 exports.get('/', (req, res) => {
   Saude.distinct('ocupacaoProfissional')
     .exec((err, data) => {
       if (err) res.send(err)
 
+      /* Mapeia os dados para permitir escalabilidade */
       let especialidades = _.map(data, function (i) {
         return { descricao: i }
       })
 
+      /* Retorna lista de especialidades */
       res.json(especialidades)
     })
 })
--- controllers/regioes.js
+++ controllers/regioes.js
@@ -3,34 +3,40 @@
 const express = require('express')
 const _ = require('lodash')
 
+/* Dados de saúde do banco de dados do Saúde Campinas */
 const Saude = require('../models/saude')
 
-var router = express.Router()
+/* Inicializa o roteamento */
+exports = express.Router()
 
-router.get('/', (req, res) => {
+/* Consulta a listagem de regiões */
+exports.get('/', (req, res) => {
   Saude.distinct('distritoAtendimento')
     .exec((err, data) => {
       if (err) res.send(err)
 
+      /* Mapeia os dados para permitir escalabilidade */
       let regioes = _.map(data, function (i) {
         return { descricao: i }
       })
 
+      /* Retorna lista de regiões */
       res.json(regioes)
     })
 })
 
-router.get('/:regiao/unidades', (req, res) => {
+/* Consulta listagem de unidades de saúde por região */
+exports.get('/:regiao/unidades', (req, res) => {
   Saude.distinct('localAtendimento', { distritoAtendimento: req.params.regiao })
     .exec((err, data) => {
       if (err) res.send(err)
 
+      /* Mapeia os dados para permitir escalabilidade */
       let unidades = _.map(data, function (i) {
         return { descricao: i }
       })
 
+      /* Retorna lista de unidades de saúde por região */
       res.json(unidades)
     })
 })
-
-module.exports = router
